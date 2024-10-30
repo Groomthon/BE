f8c94a9a1aee833d1a00e187e33f14a100535642
LEEJaeHyeok97 <hazardous10@naver.com>
2024-10-30 12:42:31 +0900

    chore: 초기 설정 추가

---
+++ src/main/java/com/jangburich/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.jangburich.domain.user.domain.repository;
+
+import com.jangburich.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
